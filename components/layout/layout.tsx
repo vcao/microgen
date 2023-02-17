9b185983e6ac00cbc71bd0163093edc3a712abf9
Victor Cao <victortcao@gmail.com>
2023-02-17 15:58:52 -0800

    first commit

---
+++ components/layout/layout.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Head from "next/head";
+import { Header } from "./header";
+import { Blocks } from "../../components/blocks-renderer";
+import { styles } from "./styles"
+import { googleFontsLink } from "./google-fonts"
+
+export const Layout = ({ rawData, children }) => {
+  const page = rawData.page
+  const global = rawData.global
+
+  return (
+    <>    
+      <Head>
+        <title>{page.meta?.title}</title>
+        <meta name="description" content={page.meta?.description} />
+        <link rel="icon" type="image/png" sizes="48x48" href={global.favicon} />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        {page.meta?.ogImage &&
+          <>
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta property="og:site_name" content={global.siteUrl} />
+            <meta property="og:url" content={global.siteUrl} />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content={page.meta?.title} />
+            <meta property="og:description" content={page.meta?.description} />
+            <meta property="og:image" content={page.meta?.siteImage} />
+          </>
+        }
+        
+        <style id="theme-styles"
+          dangerouslySetInnerHTML={{
+            __html: styles(global.theme),
+          }}
+        />
+        
+        {/* Google Analytics */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${global.gtmId}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            if (document.location.hostname.replace("www.", "") === "${global.siteUrl}") {
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${global.gtmId}', {
+                  page_path: window.location.pathname,
+                });
+              }
+            `,
+          }}
+        />
+
+        {/* Google Fonts */ }
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        {googleFontsLink(global.theme) && (
+          <link href={googleFontsLink(global.theme)} rel="stylesheet"></link>
+        )}
+      </Head>
+      <div
+        className={`flex flex-col`}
+      >
+        <Header blocks={page?.blocks} globalData={global} />
+        <div className={`flex flex-col`}>
+          {children}
+          <Blocks blocks={global.blocks} />
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ content/themes/microgen.md
@@ -0,0 +1,44 @@
+---
+name: Microgen
+desktopWidth: '1100'
+colors:
+  primary: '#0090FF'
+  accent1: '#0E4465'
+  accent2: '#1BBF6E'
+  accent3: '#DBEDF9'
+  accent4: '#F5F9FE'
+  white: '#FFFFFF'
+  grayLight: '#E8E8EF'
+  gray: '#B0B0BF'
+  grayDark: '#7B7A80'
+  black: '#000000'
+typo:
+  - label: Copy Small
+    typography: >-
+      {"family":"Inter:wght@400","size":"13","lineHeight":"26","letterSpacing":"1","margin":"22","smSize":"12","smLineHeight":"16","smLetterSpacing":"0","smMargin":"0"}
+  - label: Copy Medium
+    typography: >-
+      {"family":"Inter:wght@400","size":"15","lineHeight":"24","letterSpacing":"0","margin":"0","smSize":"15","smLineHeight":"24","smLetterSpacing":"0","smMargin":"0"}
+  - label: Copy Large
+    typography: >-
+      {"family":"Inter:wght@400","size":"21","lineHeight":"36","letterSpacing":"0","margin":"0","smSize":"21","smLineHeight":"36","smLetterSpacing":"0","smMargin":"0"}
+  - label: Headline Small
+    typography: >-
+      {"family":"Inter:wght@900","size":"14","lineHeight":"20","letterSpacing":"0","margin":"0","smSize":"14","smLineHeight":"20","smLetterSpacing":"0","smMargin":"0"}
+  - label: Headline Medium
+    typography: >-
+      {"family":"Inter:wght@900","size":"22","lineHeight":"28","letterSpacing":"0","margin":"0","smSize":"18","smLineHeight":"28","smLetterSpacing":"0","smMargin":"0"}
+  - label: Headline Large
+    typography: >-
+      {"family":"Inter:wght@900","size":"50","lineHeight":"60","letterSpacing":"0","margin":"0","smSize":"48","smLineHeight":"60","smLetterSpacing":"0","smMargin":"0"}
+buttons:
+  - label: Primary
+    fill: bg-primary opacity-100
+    typography: >-
+      {"color":"white","family":"Inter:wght@400","size":"18","lineHeight":"18","letterSpacing":"0","smSize":"18","smLineHeight":"18","smLetterSpacing":"0"}
+    padding: pt-2.5 pb-2.5 pr-9 pl-9
+    primaryBorder: border-primary border-0
+    primaryRounded: rounded
+linkColor: primary
+---
+
